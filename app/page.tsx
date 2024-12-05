7e4aa536fe2141876ab40c6c1f20236ceaa02a3c
Callum Alton <callum.alton33@gmail.com>
2024-12-05 17:06:51 +0000

    add star icon from lucide react instead of downloading whole package and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "./components/button/button";
+import { StarIcon } from "./components/star-icon/star-icon";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.root}>
       <Button price={{ amount: 1000, currency: "GBP" }} />
+      <StarIcon />
     </main>
   );
 }
