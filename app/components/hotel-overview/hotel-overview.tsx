488308f922688e38ef552b1c708af44439186a18
Callum Alton <callum.alton33@gmail.com>
2024-12-05 23:09:38 +0000

    add expanded view when read more is clicked

--- app/components/hotel-overview/hotel-overview.tsx
+++ app/components/hotel-overview/hotel-overview.tsx
@@ -8,48 +8,58 @@ import { ReadMoreLess } from "../read-more-less/read-more-less";
 import { useState } from "react";
 
 export type Image = Resort["image"];
+export type Overview = Resort["overview"];
 
 type Props = {
   image: Image;
+  overview: Overview;
 };
 
-export const HotelOverview = ({ image }: Props) => {
+export const HotelOverview = ({ image, overview }: Props) => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   return (
-    <div className={styles.root}>
-      <div className={styles["image-container"]}>
-        <Image
-          className={styles.image}
-          alt={image.description}
-          src={image.url}
-          width={200}
-          height={200}
-        />
+    <article className={styles.root}>
+      <div className={styles.wrapper}>
+        <div className={styles["image-container"]}>
+          <Image
+            className={styles.image}
+            alt={image.description}
+            src={image.url}
+            width={200}
+            height={200}
+          />
 
-        <ReadMoreLess
-          additionalClassName={styles.accordion}
-          isExpanded={isExpanded}
-          setIsExpanded={setIsExpanded}
+          <ReadMoreLess
+            additionalClassName={styles.accordion}
+            isExpanded={isExpanded}
+            setIsExpanded={setIsExpanded}
+          />
+        </div>
+        <BookingInfo
+          bookingDetails={{
+            lengthOfStay: 7,
+            party: { adults: 2, children: 2, infants: 1 },
+            price: { amount: 126, currency: "GBP" },
+          }}
+          flightDetails={{
+            departureAirport: "Manchester",
+            departureDate: new Date("2030-07-03T00:00:00.000Z"),
+          }}
+          resort={{
+            countryName: "Spain",
+            regionName: "Asturias",
+            name: "El Asturias Villa",
+            starRating: 3,
+          }}
         />
       </div>
-      <BookingInfo
-        bookingDetails={{
-          lengthOfStay: 7,
-          party: { adults: 2, children: 2, infants: 1 },
-          price: { amount: 126, currency: "GBP" },
-        }}
-        flightDetails={{
-          departureAirport: "Manchester",
-          departureDate: new Date("2030-07-03T00:00:00.000Z"),
-        }}
-        resort={{
-          countryName: "Spain",
-          regionName: "Asturias",
-          name: "El Asturias Villa",
-          starRating: 3,
-        }}
-      />
-    </div>
+      {isExpanded && (
+        <div className={styles.overview}>
+          <h2>Overview</h2>
+          <p>{overview}</p>
+        </div>
+      )}
+    </article>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <main className={styles.root}>
       <HotelOverview
+        overview={
+          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione beatae delectus nostrum! Dolorem, laborum quaerat. Omnis debitis veritatis doloremque quidem veniam quibusdam ex saepe placeat vel sequi quaerat, maiores doloribus?"
+        }
         image={{
           description: "",
           url: "https://static.onthebeach.co.uk/fe-code-test/hotel-image-1.jpg",
