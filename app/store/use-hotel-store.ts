1b449ec5dbb9ad7f3dc8a610484a4276241b9b1a
Callum Alton <callum.alton33@gmail.com>
2024-12-05 23:34:01 +0000

    add zustand to store state, avoid prop drilling, seperate the functionality from components and retain option picked on refresh

---
+++ app/store/use-hotel-store.ts
@@ -0,0 +1,68 @@
+import { create, StateCreator } from "zustand";
+import { DataTypes } from "../types/data-types";
+import { persist } from "zustand/middleware";
+
+type CurrentSort = "alphabetically" | "price" | "star rating" | "default";
+
+export type InitialHotelStoreState = {
+  results: DataTypes[] | null;
+  currentSort: CurrentSort;
+};
+
+export type UseHotelStoreState = InitialHotelStoreState & {
+  setSortResults: (results: DataTypes[], currentSort: CurrentSort) => void;
+};
+
+const initialState: InitialHotelStoreState = {
+  currentSort: "default",
+  results: null,
+};
+
+const SORTED_RESULTS_STATE = 'sorted-results'
+
+const hotelStore: StateCreator<UseHotelStoreState> = (set) => ({
+  ...initialState,
+  setSortResults: (results: DataTypes[], currentSort: CurrentSort) =>
+    set(() => {
+      let sortedResults: DataTypes[] = [];
+
+      const sortAlphabetically = [...results].sort((a, b) =>
+        a.resort.name.localeCompare(b.resort.name)
+      );
+
+      const sortByPrice = [...results].sort(
+        (a, b) => a.bookingDetails.price.amount - b.bookingDetails.price.amount
+      );
+
+      const sortByStarRating = [...results].sort(
+        (a, b) => b.resort.starRating - a.resort.starRating
+      );
+
+      switch (currentSort) {
+        case "default":
+          sortedResults = results;
+          break;
+        case "alphabetically":
+          sortedResults = sortAlphabetically;
+          break;
+        case "price":
+          sortedResults = sortByPrice;
+          break;
+        case "star rating":
+          sortedResults = sortByStarRating;
+          break;
+      }
+
+      return { results: sortedResults, currentSort };
+    }),
+});
+
+export const useHotelStore = create(
+  persist(hotelStore, {
+    name: SORTED_RESULTS_STATE,
+    partialize: (state) => ({
+      currentSort: state.currentSort,
+      results: state.results,
+    }),
+  })
+);
