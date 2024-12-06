f155d626919459592c9ca4d3ac175a290cb0a4bc
Callum Alton <callum.alton33@gmail.com>
2024-12-06 00:22:40 +0000

    use hotel list in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import { HotelOverview } from "./components/hotel-overview/hotel-overview";
+import { HotelList } from "./components/hotel-list/hotel-list";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch(
+    "https://static.onthebeach.co.uk/fe-code-test/data.json"
+  );
+
+  const results = await res.json();
+
   return (
     <main className={styles.root}>
-      <HotelOverview
-        overview={
-          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione beatae delectus nostrum! Dolorem, laborum quaerat. Omnis debitis veritatis doloremque quidem veniam quibusdam ex saepe placeat vel sequi quaerat, maiores doloribus?"
-        }
-        image={{
-          description: "",
-          url: "https://static.onthebeach.co.uk/fe-code-test/hotel-image-1.jpg",
-        }}
-      />
+      <HotelList results={results} />
     </main>
   );
 }
